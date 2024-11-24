f52dd68d2288be9ce6b73dc21fb28f45164d4cba
Ritesh Khanna <riteshkhanna@Riteshs-MacBook-Air.local>
2024-11-24 13:57:12 -0800

    index urls and database updated

--- index.php
+++ index.php
@@ -13,18 +13,43 @@ header("Content-type: application/json; charset=UTF-8");
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
-    if ($parts[2] != "users") {
-        http_response_code(404);
-        exit;
-    }
+// if ($parts[2] != "users") {
+//     http_response_code(404);
+//     exit;
+// }
 
 $id = $parts[3] ?? null;
 
 
 $database = new Database("localhost", "DevLink_db", "root", "");
 
-$gateway = new userGateway($database);
+switch ($parts[2]) {
+    case "users":
+        $gateway = new userGateway($database);
+        $controller = new UserController($gateway);
+        break;
 
-$controller = new UserController($gateway);
+    case "admin":
+        $gateway = new AdminGateway($database);
+        $controller = new AdminController($gateway);
+        break;
+
+
+    case "member":
+        $gateway = new MemberGateway($database);
+        $controller = new MemberController($gateway);
+        break;
+
+    case "follow":
+        $gateway = new FollowGateway($database);
+        $controller = new FollowController($gateway);
+        break;
+
+    default:
+        http_response_code(404);
+        exit;
+
+
+}
 
 $controller->processRequest($_SERVER["REQUEST_METHOD"], $id);
