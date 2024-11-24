5a2aab461cf1b6dfb7ef6a588f738057c038ee21
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2024-11-23 18:24:37 -0800

    Home page and HomePost component updated

--- client/src/components/HomePost.jsx
+++ client/src/components/HomePost.jsx
@@ -5,29 +5,40 @@ const HomePost = ({ post }) => {
     console.log(post);
   });
 
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return text.slice(0, maxLength - 3) + "...";
+    }
+    return text;
+  };
+
+  const maxLength = 140;
+
   return (
-    <div className="px-4 py-3 rounded bg-zinc-100 flex flex-col gap-4 max-w-96">
+    <div className="px-4 py-3 rounded bg-zinc-100 flex flex-col gap-4 w-96 drop-shadow-md">
       <div className="flex justify-between">
         <div className="flex gap-x-2 items-center">
-          <img
+          {/* TODO: To include images */}
+          {/* <img
             src={post.user.profileImage}
             alt="User profile image"
             className="w-10 h-10"
-          />
+          /> */}
           <div>
-            <h1 className="text-lg font-medium leading-4">{post.user.name}</h1>
-            <h1 className="text-gray-500">{post.user.username}</h1>
+            <h1 className="text-lg font-medium leading-4">{post.username}</h1>
+            <h1 className="text-gray-500">{post.email}</h1>
           </div>
         </div>
       </div>
 
       <div className="flex flex-col gap-y-4">
-        <p>
-          {post.content}
+        <p className="h-20">
+          {truncateText(post.content, maxLength)}
         </p>
         <p className="text-gray-500">
-          {post.timestamp}{" "}
-          <span className="font-bold text-black">{post.views}</span> Views
+          {post.datePublished}{" "}
+          {/* TODO: To include Views */}
+          {/* <span className="font-bold text-black">{post.views}</span> Views */}
         </p>
       </div>
     </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,154 +9,110 @@ const Home = () => {
     // Temporary data for testing
     const initialPosts = [
       {
-        id: 1,
-        user: {
-          name: "User Name 1",
-          username: "@username1",
-          profileImage:
-            "https://cdn-icons-png.flaticon.com/512/8847/8847419.png",
-          isFollowing: false,
-        },
-        content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis autem necessitatibus nam esse illum.`,
-        timestamp: "12:11 PM • 21/11/2024",
-        views: 987000,
-        engagement: {
-          retweets: 82000,
-          quotes: 45000,
-          likes: 91000,
-          bookmarks: 78000,
-        },
-        actions: {
-          comments: 5,
-          retweets: 5,
-          likes: 25,
-          shares: 0,
-        },
+        postID: 1,
+        userID: 1,
+        username: "@username1",
+        email: "user1@email.com",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        datePublished: "12:11 PM • 21/11/2024",
       },
       {
-        id: 2,
-        user: {
-          name: "User Name 2",
-          username: "@username2",
-          profileImage:
-            "https://cdn-icons-png.flaticon.com/512/8847/8847419.png",
-          isFollowing: true,
-        },
-        content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis autem necessitatibus nam esse illum.`,
-        timestamp: "1:45 PM • 21/11/2024",
-        views: 245000,
-        engagement: {
-          retweets: 60000,
-          quotes: 25000,
-          likes: 71000,
-          bookmarks: 50000,
-        },
-        actions: {
-          comments: 8,
-          retweets: 12,
-          likes: 50,
-          shares: 3,
-        },
+        postID: 2,
+        userID: 2,
+        username: "@username2",
+        email: "user2@email.com",
+        content:
+          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        datePublished: "1:45 PM • 21/11/2024",
       },
       {
-        id: 3,
-        user: {
-          name: "User Name 3",
-          username: "@username3",
-          profileImage:
-            "https://cdn-icons-png.flaticon.com/512/8847/8847419.png",
-          isFollowing: false,
-        },
-        content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis autem necessitatibus nam esse illum.`,
-        timestamp: "2:30 PM • 21/11/2024",
-        views: 123456,
-        engagement: {
-          retweets: 8000,
-          quotes: 4500,
-          likes: 9100,
-          bookmarks: 7800,
-        },
-        actions: {
-          comments: 1,
-          retweets: 1,
-          likes: 5,
-          shares: 0,
-        },
+        postID: 3,
+        userID: 3,
+        username: "@username3",
+        email: "user3@email.com",
+        content:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        datePublished: "2:30 PM • 21/11/2024",
       },
       {
-        id: 4,
-        user: {
-          name: "User Name 4",
-          username: "@username4",
-          profileImage:
-            "https://cdn-icons-png.flaticon.com/512/8847/8847419.png",
-          isFollowing: false,
-        },
-        content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis autem necessitatibus nam esse illum.`,
-        timestamp: "3:00 PM • 21/11/2024",
-        views: 654321,
-        engagement: {
-          retweets: 12000,
-          quotes: 35000,
-          likes: 45000,
-          bookmarks: 33000,
-        },
-        actions: {
-          comments: 10,
-          retweets: 15,
-          likes: 100,
-          shares: 5,
-        },
+        postID: 4,
+        userID: 4,
+        username: "@username4",
+        email: "user4@email.com",
+        content:
+          "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        datePublished: "3:10 PM • 21/11/2024",
       },
       {
-        id: 5,
-        user: {
-          name: "User Name 5",
-          username: "@username5",
-          profileImage:
-            "https://cdn-icons-png.flaticon.com/512/8847/8847419.png",
-          isFollowing: true,
-        },
-        content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis autem necessitatibus nam esse illum.`,
-        timestamp: "4:15 PM • 21/11/2024",
-        views: 321654,
-        engagement: {
-          retweets: 15000,
-          quotes: 7000,
-          likes: 21000,
-          bookmarks: 19000,
-        },
-        actions: {
-          comments: 15,
-          retweets: 20,
-          likes: 75,
-          shares: 7,
-        },
+        postID: 5,
+        userID: 5,
+        username: "@username5",
+        email: "user5@email.com",
+        content:
+          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+        datePublished: "4:05 PM • 21/11/2024",
       },
       {
-        id: 6,
-        user: {
-          name: "User Name 6",
-          username: "@username6",
-          profileImage:
-            "https://cdn-icons-png.flaticon.com/512/8847/8847419.png",
-          isFollowing: false,
-        },
-        content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis autem necessitatibus nam esse illum.`,
-        timestamp: "5:00 PM • 21/11/2024",
-        views: 100000,
-        engagement: {
-          retweets: 10000,
-          quotes: 5000,
-          likes: 15000,
-          bookmarks: 14000,
-        },
-        actions: {
-          comments: 3,
-          retweets: 2,
-          likes: 10,
-          shares: 1,
-        },
+        postID: 6,
+        userID: 6,
+        username: "@username6",
+        email: "user6@email.com",
+        content:
+          "Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+        datePublished: "5:20 PM • 21/11/2024",
+      },
+      {
+        postID: 7,
+        userID: 7,
+        username: "@username7",
+        email: "user7@email.com",
+        content:
+          "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.",
+        datePublished: "6:45 PM • 21/11/2024",
+      },
+      {
+        postID: 8,
+        userID: 8,
+        username: "@username8",
+        email: "user8@email.com",
+        content:
+          "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.",
+        datePublished: "7:30 PM • 21/11/2024",
+      },
+      {
+        postID: 9,
+        userID: 9,
+        username: "@username9",
+        email: "user9@email.com",
+        content:
+          "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.",
+        datePublished: "8:15 PM • 21/11/2024",
+      },
+      {
+        postID: 10,
+        userID: 10,
+        username: "@username10",
+        email: "user10@email.com",
+        content: "Et harum quidem rerum facilis est et expedita distinctio.",
+        datePublished: "9:00 PM • 21/11/2024",
+      },
+      {
+        postID: 11,
+        userID: 11,
+        username: "@username11",
+        email: "user11@email.com",
+        content:
+          "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat.",
+        datePublished: "10:35 PM • 21/11/2024",
+      },
+      {
+        postID: 12,
+        userID: 12,
+        username: "@username12",
+        email: "user12@email.com",
+        content: "Omnis voluptas assumenda est, omnis dolor repellendus.",
+        datePublished: "11:10 PM • 21/11/2024",
       },
     ];
 
@@ -165,15 +121,15 @@ const Home = () => {
 
   return (
     <div className="p-6">
-      <div>
-        <h1 className="text-4xl">This is home</h1>
-        <h3 className="text-2xl">Subheading</h3>
+      <div className="text-center py-10">
+        <h1 className="text-4xl font-extrabold text-gray-800">DevLink</h1>
+        <h3 className="text-2xl font-thin">Explore</h3>
       </div>
 
-      <ul className="p-4 flex flex-wrap gap-4">
+      <ul className="p-4 flex flex-wrap gap-4 justify-center">
         {posts.map((post) => (
-          <li key={post.id}>
-            <Link to={`/ViewPost/${post.id}`}>
+          <li key={post.postID}>
+            <Link to={`/ViewPost/${post.postID}`}>
               <HomePost post={post} />
             </Link>
           </li>
@@ -184,4 +140,3 @@ const Home = () => {
 };
 
 export default Home;
-
